8842c0762f01f9a035801310f5559f2a89c8b4ef
Ephraim-Cheaye-22 <833955@mystma.org>
2021-09-22 09:16:21 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + " " + b + " " + c + " " + d + " " + e + " "
+
+print(topFiveMovies("Matrix ", "Deadpool ", "Dark Knight ", "Spider man ", "Iron Man "))
